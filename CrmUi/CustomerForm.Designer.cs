012c3f8fd5185f14d0811e30ea935f009043e02c
Дмитрий <dima-tkachenko@mail.ru>
2020-02-08 16:34:21 +0300

    добавлены файлы

--- CrmUi/CustomerForm.Designer.cs
+++ CrmUi/CustomerForm.Designer.cs
@@ -54,6 +54,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Location = new System.Drawing.Point(164, 178);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
