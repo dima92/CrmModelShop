c78794a18495b6607e026dbfe7403c1ec1348916
Дмитрий <dima-tkachenko@mail.ru>
2020-02-08 22:13:20 +0300

    Добавлена корзина

--- CrmBl/Model/Product.cs
+++ CrmBl/Model/Product.cs
@@ -14,5 +14,20 @@ namespace CrmBl.Model
         {
             return Name;
         }
+
+        public override int GetHashCode()
+        {
+            return ProductId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product product)
+            {
+                return ProductId.Equals(product.ProductId);
+            }
+
+            return false;
+        }
     }
 }
